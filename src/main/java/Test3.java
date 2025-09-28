881077add19d4e1174105cb8ae9aceca6953e48a
roman <velkoromvelkorom@gmail.com>
2025-09-28 15:22:31 +0200

    Add task 3 changes

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,5 +1,6 @@
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Разработка ветки. Задача 3");
+        System.out.println("Разработка ветки. Задача 3 Исправленние");
     }
 }
